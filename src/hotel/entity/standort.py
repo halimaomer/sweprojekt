905b4d272fa7d5bce0b75ef77797cbfaf8e4ee3a
alisaknbl <knal1011@h-ka.de>
2026-03-30 22:34:36 +0200

    Create standort.py

---
+++ src/hotel/entity/standort.py
@@ -0,0 +1,44 @@
+"""Entity-Klasse für den Standort."""
+
+from sqlalchemy import ForeignKey, Identity
+from sqlalchemy.orm import Mapped, mapped_column
+
+from hotel.entity.base import Base
+
+
+class Standort(Base):
+    """Entity-Klasse für den Standort."""
+
+    __tablename__ = "standort"
+
+    strasse: Mapped[str]
+    """Die Straße."""
+
+    hausnummer: Mapped[str]
+    """Die Hausnummer."""
+
+    plz: Mapped[str]
+    """Die Postleitzahl."""
+
+    ort: Mapped[str]
+    """Der Ort."""
+
+    land: Mapped[str]
+    """Das Land."""
+
+    id: Mapped[int] = mapped_column(
+        Identity(start=1000),
+        primary_key=True,
+    )
+    """Die generierte ID gemäß der zugehörigen IDENTITY-Spalte."""
+
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotel.id"))
+    """ID des zugehörigen Hotels als Fremdschlüssel in der DB-Tabelle."""
+
+    def __repr__(self) -> str:
+        """Ausgabe eines Standorts als String ohne die Hoteldaten."""
+        return (
+            f"Standort(id={self.id}, "
+            f"strasse={self.strasse}, hausnummer={self.hausnummer}, "
+            f"plz={self.plz}, ort={self.ort}, land={self.land})"
+        )
